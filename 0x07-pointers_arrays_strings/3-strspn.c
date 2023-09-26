e72e29ef9c858c002bc36e82cc180f9605a9474a
Josh19744 <olotujoshuaolaoluwa@gmail.com>
2023-09-26 07:36:09 -0700

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,7 +12,7 @@ value = 0;
 for (i = 0; s[i] != '\0'; i++)
 {
 check = 0;
-for (n = 0; accept[n] != '\0' n++)
+for (n = 0; accept[n] != '\0'; n++)
 {
 if (accept[n] == s[i])
 {
@@ -20,4 +20,10 @@ value++;
 check = 1;
 }
 }
-
+if (check == 0)
+{
+return (value);
+}
+}
+return (0);
+}
